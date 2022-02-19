a4a1634b16c125cd624cb7d440efd7ecef8545b7
KULkaur <kulbirk347@gmail.com>
2022-02-19 11:08:35 -0500

    Initial commit of JS work

---
+++ script.js
@@ -0,0 +1,24 @@
+document.writeln("massod from java script!");
+document.writeln("23")
+
+
+//data--abkcjbbhhh
+// numbers--1,2,3,4,5
+//decimal point number -- 1.2,3.1 float--double-decimal
+// text-massod
+//boolean true false.
+
+
+
+// var-- variant
+document.writeln("")
+number= 50;
+var firstname ="massod" 
+var first_num =  23;
+var sec_num = 41;
+
+document.writeln(number);
+document.writeln(firstname);
+document.writeln("substraction is: (fist_num- sec_num)+ <br>");
+ var a = 5;
+document.writeln(a++)
